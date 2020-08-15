d6db0d22fc1ce551497271cc3d00e9356bdb4e1e
kevin alexander <kevinalexander78962106@gmail.com>
2020-08-15 09:07:15 -0600

    Update CLASE1.java

--- CLASE1/src/clase1/CLASE1.java
+++ CLASE1/src/clase1/CLASE1.java
@@ -1,22 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clase1;
 
-/**
- *
- * @author kevin
- */
-public class CLASE1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("HOLA GITHUB");
-    }
+public class CLASE1 {
+    
+    
+  
+    
+    
     
 }
+
+  
+    
+    public static void main(String[] args) {
+
+
+  funcionesMatematicaSuma Funciones = new funcionesMatematicaSuma ();
+       
+        
+
+
+
+
+
+
+  
+ 

6b4d1d47186940cecc27fb7a41939f4d07d6ebb6
kevin alexander <kevinalexander78962106@gmail.com>
2020-08-18 20:29:47 -0600

    trabajo terminado

--- com.unab.clase02/src/main/java/com/unab/clase02/Lbros.java
+++ com.unab.clase02/src/main/java/com/unab/clase02/Lbros.java
@@ -1,22 +1,68 @@
+package com.unab.clase02;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.unab.clase02;
 
 /**
  *
  * @author kevin
  */
-class Lbros {
+public class Lbros {
+    
+    private int Id;
+    private String NombreLbros;
+    private int NumeroPaginas;
+    private String Autor;
+    
+   public Lbros(int Id,String NumeroLbros,int NumeroPaginas,String Autor){
+   
+   this.Id = Id;
+   this.NombreLbros = NumeroLbros;
+   this.NumeroPaginas = NumeroPaginas;
+   this.Autor = Autor;
+   
 
-    Lbros(int i, String programacion_java, int i0, String deitel) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
+   //get se utiliza para recuperar informacion 
+    public int getId() {
+        return Id;
+    }
+    //set se utiliza para modificar informacion 
+    public void setId(int Id) {
+        this.Id = Id;
     }
 
-    boolean getNumeroPaginas() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String getNombreLbros() {
+        return NombreLbros;
     }
+
+    public void setNombreLbros(String NombreLbros) {
+        this.NombreLbros = NombreLbros;
+    }
+
+    public int getNumeroPaginas() {
+        return NumeroPaginas;
+    }
+
+    public void setNumeroPaginas(int NumeroPaginas) {
+        this.NumeroPaginas = NumeroPaginas;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Lbros{" + "Id=" + Id + ", NombreLbros=" + NombreLbros + ", NumeroPaginas=" + NumeroPaginas + ", Autor=" + Autor + '}';
+    }
+   
     
 }
